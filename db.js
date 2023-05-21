bf598b2c39892a17c970e9ccc7bb4dc14b057f72
Okan <okanboyraz8@gmail.com>
2023-05-21 22:23:43 +0300

    Initial commit

---
+++ db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const conn = () => {
+    mongoose.connect(process.env.DB_URI, {
+        dbName: 'lenslight_tr',
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("Connected to the DB succesfully");
+    }).catch((err) => {
+        console.log(`DB connection err:, ${err}`);
+    })
+};
+
+export default conn;
